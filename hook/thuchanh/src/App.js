16dc4ef53d4f74f83cd3a28eda65dd9128793ac2
nguyenngoc1234567 <ngoc2503pp@gmail.com>
2023-04-03 17:16:14 +0700

    thuchanh

---
+++ hook/thuchanh/src/App.js
@@ -0,0 +1,22 @@
+// import Counter from "./components/Counter";
+// import MyClock from "./components/MyClock";
+// import "./App.css";
+// function App() {
+//   return
+
+// <Counter />;
+// }
+
+// export default App;
+import MyClock from "./components/MyClock";
+import "./App.css";
+
+function App() {
+  return (
+    <div class="Container">
+      <MyClock />
+    </div>
+  );
+}
+
+export default App;
