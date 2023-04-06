a11ee067d27f7527acde8f6b597f174248cfdec8
nguyenngoc1234567 <ngoc2503pp@gmail.com>
2023-04-06 14:30:27 +0700

    thuchanh2

---
+++ netjs/thuchanh2/src/pages/mock-data/data.js
@@ -0,0 +1,30 @@
+const students = [
+    {
+      id: 1,
+      name: "ngọc nè "
+    },
+    {
+      id: 2,
+      name: "nho nè "
+    },
+    {
+      id: 3,
+      name: "phong nè "
+    },
+    {
+      id: 4,
+      name: "tâm nè "
+    },
+    {
+      id: 5,
+      name: "nho nè2"
+    },
+    {
+      id: 6,
+      name: "phong nè 2"
+    }
+  ];
+  
+  export function getStudents() {
+    return students;
+  }
